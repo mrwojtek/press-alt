bb5304a2e8be14b4786c7fc4934ba24b021df9c3
Wojciech Mruczkiewicz <mrwojtek@mrwojtek.pl>
2016-04-07 13:00:27 +0200

    Add a script to extract NMEA data

---
+++ scripts/extract-nmea.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  (C) Copyright 2013, 2016 Wojciech Mruczkiewicz
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import argparse
+import pressalt
+
+
+def read_file(file, reader, legacy):
+    if file.endswith('.log') or file.endswith('.txt'):
+        return pressalt.read_text(file, reader)
+    else:
+        return pressalt.read_binary(file, reader, legacy)
+
+parser = argparse.ArgumentParser(description='Convert binary recording to the text file.')
+parser.add_argument('file', type=str, help='File with a binary recording')
+parser.add_argument('-o', '--output', dest='output',
+                    help='Output file, writing to standard output when missing')
+parser.add_argument('--legacy', action='store_true', help='Use legacy binary mode.')
+args = parser.parse_args()
+
+if args.output is not None:
+    with open(args.output, 'w') as f:
+        pressalt.read_binary(args.file, pressalt.RecordNmeaToText(file=f))
+else:
+    read_file(args.file, pressalt.RecordNmeaToText(), args.legacy)
